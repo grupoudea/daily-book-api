db86d1c55a2d2a5f596cf883e3a4fe55bcff7799
kellytbn <kellytbn14@gmail.com>
2024-09-30 22:36:00 -0500

    Inicio de proyecto

---
+++ src/main/java/com/ritallus/dailybookapi/DailyBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.ritallus.dailybookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyBookApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyBookApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ritallus/dailybookapi/DailyBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritallus.dailybookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyBookApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
